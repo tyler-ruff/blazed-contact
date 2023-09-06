8fd0a86191ea5a83e8f252c542022f5c158484a9
Tyler Ruff <hello@blazed.space>
2023-09-06 16:38:43 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 module.exports = {
+    content: [
+        './assets/**/*.{vue,js,css}',
+        './components/**/*.{vue,js}',
+        './layouts/**/*.vue',
+        './pages/**/*.vue',
+        './server/**/*.{js,ts}',
+        './plugins/**/*.{js,ts}',
+        './nuxt.config.{js,ts}',
+        './node_modules/windplus/styles/**/*.{vue,js,css}',
+        './node_modules/windplus/styles/*.{vue,js,css}',
+        './node_modules/windplus/styles/main.css',
+    ],
     plugins: [
         require('@tailwindcss/typography'),
         require('@tailwindcss/forms'),
         require('daisyui')
     ],
-};
-  
+}
